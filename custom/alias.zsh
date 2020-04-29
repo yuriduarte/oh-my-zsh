c92d47551a60e2f21233d3c7dac04626d6983510
Yuri Duarte <yuriduartedev@gmail.com>
2020-04-29 12:18:38 -0300

    Adiciona alias, plugins e themas no github

---
+++ custom/alias.zsh
@@ -0,0 +1,107 @@
+alias ll="ls -Glahs"
+alias ls="/bin/ls -G"
+alias psgrep="ps aux | egrep -v egrep | egrep"
+
+# alias edit="emacs"
+function edit() {
+  emacs $1 &
+}
+
+# alias edit="mvim"
+
+alias editzsh="edit ~/.zsh.d/"
+alias goemacs="cd ~/.emacs.d/"
+alias editemacs="edit ~/.emacs.d/"
+
+# Elixir
+alias mt="mix test"
+alias mts="mix test --stale"
+alias mtd="mix test.debug"
+alias mta="mix test.all"
+alias mtf="mix test.feature"
+alias mtds="mix test.debug --stale"
+alias mps="mix phx.server"
+alias mpr="mix phx.routes"
+alias mted="MIX_ENV=test mix ecto.drop"
+alias mdg="mix deps.get"
+
+# Ruby
+alias be="bundle exec"
+alias rails="be rails"
+alias rake="be rake"
+alias rdm="rake db:migrate"
+alias rdc="rake db:create"
+alias rs="rails s -b 0.0.0.0"
+alias sidekiq="be sidekiq"
+
+# GIT
+alias gs="git status"
+alias gb="git branch"
+alias gba="git branch -a"
+alias gm="git merge --no-ff"
+alias gp="git push -u && git push --tags"
+alias gpm="git push origin master"
+alias gf="git fetch origin -p"
+alias glog="git log --graph"
+alias gpr="git pull --rebase"
+alias gc="git checkout"
+alias gcm="git checkout master"
+alias gcd="git checkout develop"
+
+function gcf() {
+  git checkout feature/$1
+}
+
+alias gffs="git flow feature start"
+alias gfff="git flow feature finish"
+alias gffp="git flow feature publish"
+alias gfft="git flow feature track"
+
+
+# ASDF
+alias redis="redis-server . &"
+alias postgres="pg_ctl start"
+alias kpostgres="pg_ctl stop"
+
+# React Native
+alias rn="react-native"
+alias rn-ios="rn run-ios"
+alias rn-android="rn run-android"
+alias rn-start="npm start -- --reset-cache"
+
+# Bash
+alias copy_key="cat ~/.ssh/id_rsa.pub | pbcopy"
+
+# Ember
+alias es="ember s"
+
+# Gatsby
+alias gad="gatsby develop"
+alias gab="gatsby build"
+
+# Commons
+alias rmds="find . -name '.DS_Store' -type f -delete"
+
+# Easy folder and file loading
+alias projects="cd ~/Projects"
+alias pgc="cd ~/Projects/grupo-cataratas"
+alias cursos="cd ~/Projects/cursos"
+alias aquario="cd ~/Projects/grupo-cataratas/aquario"
+alias bioparque="cd ~/Projects/grupo-cataratas/bioparque"
+alias holding="cd ~/Projects/grupo-cataratas/holding"
+alias instituto="cd ~/Projects/grupo-cataratas/instituto"
+alias paineiras="cd ~/Projects/grupo-cataratas/paineiras"
+alias dl="cd ~/Downloads"
+alias dt="cd ~/Desktop"
+alias apps="/Applications"
+alias fl="$DOTFILES"
+
+# Show the external IP
+alias myip="dig +short myip.opendns.com @resolver1.opendns.com"
+
+# IP addresses
+alias localip="ipconfig getifaddr en1"
+alias ips="ifconfig -a | perl -nle'/(\d+\.\d+\.\d+\.\d+)/ && print $1'"
+
+# Campainha
+alias start-camp="pg_ctl start && redis-server & mps"
---
+++ custom/export.zsh
@@ -0,0 +1,33 @@
+PATH=/usr/local/heroku/bin:$PATH
+export PATH=$PATH
+
+export EDITOR="nano"
+
+# brew/cask
+# export HOMEBREW_CASK_OPTS="--caskroom=~/Applications"
+
+# IONIC / CORDOVA
+# export JAVA_HOME=$(/usr/libexec/java_home)
+
+# Using Android Studio path
+# export ANDROID_HOME=~/Library/Android/sdk
+# export PATH=${PATH}:$ANDROID_HOME/tools:$ANDROID_HOME/platform-tools
+
+# export PATH="/usr/local/sbin:$PATH"
+
+# export PATH=$(brew --prefix openssl)/bin:$PATH
+
+# export PATH="/usr/local/opt/imagemagick@6/bin:$PATH"
+
+# VSCode
+export PATH="$PATH:/Applications/Visual Studio Code.app/Contents/Resources/app/bin"
+
+# Node
+export PATH="/usr/local/share/npm/bin:$PATH"
+export PATH=$PATH:~/.asdf/installs/nodejs/10.13.0/.npm/bin/
+
+export PATH="/Users/yuriduarte/.asdf/installs/nodejs/8.9.4/.npm/bin:$PATH"
+export PATH="~/Users/yuriduarte/.asdf/installs/nodejs/10.13.0/.npm/bin:$PATH"
+export PATH="~/.asdf/installs/nodejs/8.9.1/.npm/bin:$PATH"
+export PATH="~/.asdf/installs/nodejs/8.9.4/.npm/bin:$PATH"
+export PATH="~/.asdf/installs/nodejs/10.13.0/.npm/bin:$PATH"
