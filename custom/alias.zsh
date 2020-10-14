cce4ba986049a397bc50554e6fe85711243231b2
Yuri Duarte <yuriduartedev@gmail.com>
2020-10-14 08:52:46 -0300

    Atualiza alias

--- custom/alias.zsh
+++ custom/alias.zsh
@@ -121,6 +121,8 @@ alias myip="dig +short myip.opendns.com @resolver1.opendns.com"
 alias localip="ipconfig getifaddr en1"
 alias ips="ifconfig -a | perl -nle'/(\d+\.\d+\.\d+\.\d+)/ && print $1'"
 
+alias prmps="pg_ctl start && redis-server & mps"
+
 # Campainha
 alias start-camp="pg_ctl start && redis-server & code . && mps"
 alias ssh-camp-test="ssh ubuntu@srv-site-test.campainha.com.br"
